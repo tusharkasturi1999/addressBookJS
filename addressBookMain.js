d5c3f156e53f39d34f15c53085d0020ceb97325c
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-10-03 19:20:27 +0530

    Initial Commit

---
+++ addressBookMain.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")();
+
+let contact;
+const firstName = prompt("Enter the first name: ");
+const lastName = prompt("Enter the last name: ");
+const address = prompt("Enter the Address: ");
+const city = prompt("Enter the city: ");
+const state = prompt("Enter the State: ");
+const zip = prompt("Enter the ZIP: ");
+const phone = prompt("Enter the Phone number: ");
+const email = prompt("Enter the email id: ");
+
+contact = {
+  firstName: firstName,
+  lastName: lastName,
+  address: address,
+  city: city,
+  state: state,
+  zip: zip,
+  phone: phone,
+  email: email,
+};
+console.log(contact);
