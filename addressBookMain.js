92e1c0da827e8d22dfb8d8b724461228c5d96ba0
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-10-03 19:59:36 +0530

    Added: validate regex functions

--- addressBookMain.js
+++ addressBookMain.js
@@ -1,24 +1,41 @@
 const prompt = require("prompt-sync")();
+const validation = require("./validation.js");
 
 let contact;
-const firstName = prompt("Enter the first name: ");
-const lastName = prompt("Enter the last name: ");
-const address = prompt("Enter the Address: ");
-const city = prompt("Enter the city: ");
-const state = prompt("Enter the State: ");
-const zip = prompt("Enter the ZIP: ");
-const phone = prompt("Enter the Phone number: ");
-const email = prompt("Enter the email id: ");
+let addContact = () => {
+  let firstName = prompt("Enter the first name: ");
+  let lastName = prompt("Enter the last name: ");
+  let address = prompt("Enter the Address: ");
+  let city = prompt("Enter the city: ");
+  let state = prompt("Enter the State: ");
+  let zip = prompt("Enter the ZIP: ");
+  let phone = prompt("Enter the Phone number: ");
+  let email = prompt("Enter the email id: ");
+  try {
+    validation.firstNameValidation(firstName);
+    validation.lastNameValidation(lastName);
+    validation.addressValidation(address);
+    validation.cityValidation(city);
+    validation.stateValidation(state);
+    validation.zipValidation(zip);
+    validation.phoneValidation(phone);
+    validation.emailValidation(email);
+  } catch (error) {
+    console.error(error);
+    addContact();
+  }
 
-contact = {
-  firstName: firstName,
-  lastName: lastName,
-  address: address,
-  city: city,
-  state: state,
-  zip: zip,
-  phone: phone,
-  email: email,
+  contact = {
+    firstName: firstName,
+    lastName: lastName,
+    address: address,
+    city: city,
+    state: state,
+    zip: zip,
+    phone: phone,
+    email: email,
+  };
+  console.log("Contacts inserted successfully");
+  console.log(contact);
 };
-console.log(contact);
- 
+addContact();
